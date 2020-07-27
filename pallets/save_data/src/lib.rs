008b1f193fbe72552dbb5f72a6ebb016e7c23357
shin.takahashi <shin.takahashi@fintertech.co.jp>
2020-07-27 20:43:19 +0900

    declare struct

--- pallets/save_data/src/lib.rs
+++ pallets/save_data/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode};
 /// A FRAME pallet template with necessary imports
 
 /// Feel free to remove or edit this file as needed.
@@ -11,11 +12,42 @@
 use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
 use frame_system::{self as system, ensure_signed};
 
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
+
+#[derive(Clone, Eq, PartialEq, Default, Encode, Decode, Hash)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+pub struct PrivateData {
+    pub my_number: Vec<u8>,
+    pub driver_license_number: Vec<u8>,
+    pub medical_number: Vec<u8>,
+}
+#[derive(Clone, Eq, PartialEq, Default, Encode, Decode, Hash)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+pub struct PublicData {
+    pub name: Vec<u8>,
+    pub address: Vec<u8>,
+    pub sexality: u8,
+    pub job: Vec<u8>,
+    pub company: Vec<u8>,
+}
+#[derive(Clone, Eq, PartialEq, Default, Encode, Decode, Hash)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+pub struct CognitiveInfo<AccountId, Moment> {
+    pub account_id: AccountId,
+    pub public_data: PublicData,
+    pub private_data: PrivateData,
+    pub register_block_number: Moment,
+}
+
+pub type CognitiveInfoOf<T> =
+    CognitiveInfo<<T as frame_system::Trait>::AccountId, <T as frame_system::Trait>::BlockNumber>;
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait {
@@ -31,10 +63,8 @@ decl_storage! {
     // storage items are isolated from other pallets.
     // ---------------------------------vvvvvvvvvvvvvv
     trait Store for Module<T: Trait> as TemplateModule {
-        // Just a dummy storage item.
-        // Here we are declaring a StorageValue, `Something` as a Option<u32>
-        // `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
         Something get(fn something): Option<u32>;
+        pub CognitiveInfos: map hasher(blake2_128_concat) T::AccountId => Option<CognitiveInfoOf<T>>;
     }
 }
 
